caec0099c656ce1c3906db2ca18352dc6215a152
lucas <lucas@home.home>
2023-06-05 18:51:16 +0200

    fitclub

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,7 @@ modal_overlay.addEventListener("click", () =>{
 
 prev_btn.addEventListener("click", () => {
     if (currentIndex === 0) {
-        currentIndex = 6      
+        currentIndex = 7      
     }else {
         currentIndex--;
     }
@@ -106,7 +106,7 @@ prev_btn.addEventListener("click", () => {
 });
 
 next_btn.addEventListener("click", () => {
-    if (currentIndex === 6) {
+    if (currentIndex === 7) {
         currentIndex = 0     
     }else {
         currentIndex++;
