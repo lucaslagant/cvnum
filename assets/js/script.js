0e2c7420a6668e1375cedd32f9b0d1c8b6b8dc17
lucaslagant <lucaslagant@gmail.com>
2023-09-25 11:17:02 +0200

    add besmart portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -99,7 +99,7 @@ modal_overlay.addEventListener("click", () =>{
 
 prev_btn.addEventListener("click", () => {
     if (currentIndex === 0) {
-        currentIndex = 9      
+        currentIndex = 10      
     }else {
         currentIndex--;
     }
@@ -107,7 +107,7 @@ prev_btn.addEventListener("click", () => {
 });
 
 next_btn.addEventListener("click", () => {
-    if (currentIndex === 9) {
+    if (currentIndex === 10) {
         currentIndex = 0     
     }else {
         currentIndex++;
